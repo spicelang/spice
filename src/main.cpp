0015ae31cf8ac350cf1d8ee1f28a5f69662464f4
Marc Auberer <marc.auberer@sap.com>
2021-07-01 09:07:48 +0200

    Follow other guide

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,14 @@
 // Copyright (c) 2021 ChilliBits. All rights reserved.
 
+#include <iostream>
 #include "antlr4-runtime.h"
-#include "antlr/SpiceLexer.h"
-#include "antlr/SpiceParser.h"
-#include "analyzer/AnalyzerVisitor.h"
+//#include "antlr/SpiceLexer.h"
+//#include "antlr/SpiceParser.h"
+//#include "analyzer/AnalyzerVisitor.h"
 
 int main(int argc, char** argv) {
     // Read from file
-    std::ifstream stream;
+    /*std::ifstream stream;
     stream.open(argv[1]);
     antlr4::ANTLRInputStream input(stream);
 
@@ -18,8 +19,9 @@ int main(int argc, char** argv) {
 
     // Execute syntactical analysis
     antlr4::tree::ParseTree *tree = parser.entry();
-    AnalyzerVisitor().visit(tree);
+    AnalyzerVisitor().visit(tree);*/
 
     // Return with positive result code
+    std::cout << "Hello world" << std::endl;
     return 0;
 }
