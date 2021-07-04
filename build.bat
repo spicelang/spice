5329a72d61f8d7833e4598d26b71d947829e88ca
Marc Auberer <marc.auberer@sap.com>
2021-07-04 15:10:16 +0200

    Fix C++ CI workflow

--- build.bat
+++ build.bat
@@ -2,8 +2,8 @@
 mkdir bin 2> NUL
 cd bin
 
-cmake -DCMAKE_BUILD_TYPE=Release -G "MinGW Makefiles" ../compiler
-mingw32-make
+cmake -DCMAKE_BUILD_TYPE=Release -G "CodeBlocks - MinGW Makefiles" ../compiler
+cmake --build . --target Spice_run -- -j 6
 
 move src\Spice_run.exe spicec.exe
 
