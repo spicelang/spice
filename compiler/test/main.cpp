4e20cb83947dacd3f722fe9c86af2189d7bf622b
Marc Auberer <marc.auberer@chillibits.com>
2021-07-24 00:31:33 +0200

    Add basic test module structure

---
+++ compiler/test/main.cpp
@@ -0,0 +1,8 @@
+// Copyright (c) 2021 ChilliBits. All rights reserved.
+
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
