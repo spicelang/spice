82b161e2bc31d3367ce998ffe0d78bce92bd98f0
Marc Auberer <marc.auberer@chillibits.com>
2021-11-15 21:10:27 +0100

    Cli refactoring

--- cli/cmd/run.go
+++ cli/cmd/run.go
@@ -7,10 +7,35 @@ import (
 	"runtime"
 	"spice/util"
 	"strings"
+
+	"github.com/urfave/cli/v2"
 )
 
+// RunCliFlags are the cli flags for the run command
+var RunCliFlags = []cli.Flag{
+	&cli.BoolFlag{
+		Name:    "debug-output",
+		Aliases: []string{"d"},
+		Usage:   "Print compiler output for debugging",
+		Value:   false,
+	},
+	&cli.IntFlag{
+		Name:    "opt-level",
+		Aliases: []string{"o"},
+		Usage:   "Set optimization level",
+		Value:   2,
+	},
+}
+
+// ---------------------------------------------------------------- Public functions ---------------------------------------------------------------
+
 // Run takes the passed code file, resolves its dependencies, emits an executable and runs it
-func Run(sourceFile string, debugOutput bool, optLevel int) {
+func Run(c *cli.Context) error {
+	// Extract flags
+	sourceFile := c.Args().Get(0)
+	debugOutput := c.Bool("debug-output")
+	optLevel := c.Int("opt-level")
+
 	sourceFileName := filepath.Base(sourceFile)
 	sourceFileNameWithoutExt := strings.TrimSuffix(sourceFileName, filepath.Ext(sourceFileName))
 
@@ -20,18 +45,20 @@ func Run(sourceFile string, debugOutput bool, optLevel int) {
 		buildPath = os.TempDir()
 		if err := os.MkdirAll(buildPath, 0750); err != nil {
 			util.Error("Could not find output dir", true)
+			return err
 		}
 		buildPath += "\\spice-executable.exe"
 	} else if runtime.GOOS == "linux" {
 		buildPath = os.TempDir()
 		if err := os.MkdirAll(buildPath, 0750); err != nil {
 			util.Error("Could not find output dir", true)
+			return err
 		}
 		buildPath += "/spice-executable"
 	}
 
 	// Compile program and emit executable file to tmp dir
-	Build(sourceFile, "", "", "", buildPath, debugOutput, optLevel)
+	buildFromSourceFile(sourceFile, "", "", "", buildPath, debugOutput, optLevel)
 
 	// Run executable
 	cmd := exec.Command(buildPath)
@@ -45,4 +72,5 @@ func Run(sourceFile string, debugOutput bool, optLevel int) {
 	if err != nil {
 		util.Error("Could not run executable", true)
 	}
+	return nil
 }
--- cli/spice.go
+++ cli/spice.go
@@ -25,84 +25,21 @@ func main() {
 		Usage:   "Prints the Spice version, you're running",
 	}
 
-	// Commands
-	buildCommand := cli.Command{
-		Name:    "build",
-		Aliases: []string{"b"},
-		Usage:   "Builds your Spice program and emits an executable",
-		Flags: []cli.Flag{
-			&cli.BoolFlag{Name: "debug-output", Aliases: []string{"d"}, Usage: "Print compiler output for debugging", Value: false},
-			&cli.StringFlag{Name: "target-arch", Aliases: []string{"ta"}, Usage: "Target CPU architecture for the emitted executable (for cross-compiling)"},
-			&cli.StringFlag{Name: "target-vendor", Aliases: []string{"tv"}, Usage: "Target vendor for the emitted executable (for cross-compiling)"},
-			&cli.StringFlag{Name: "target-os", Aliases: []string{"to"}, Usage: "Target os for the emitted executable (for cross-compiling)"},
-			&cli.IntFlag{Name: "opt-level", Aliases: []string{"o"}, Usage: "Set optimization level", Value: 2},
-			&cli.PathFlag{Name: "output", Usage: "Path to the location where the output executable should go"},
-		},
-		Action: func(c *cli.Context) error {
-			cmd.Build(
-				c.Args().Get(0),
-				c.String("target-arch"),
-				c.String("target-vendor"),
-				c.String("target-os"),
-				c.Path("output"),
-				c.Bool("debug-output"),
-				c.Int("opt-level"),
-			)
-			return nil
-		},
-	}
-	installCommand := cli.Command{
-		Name:    "install",
-		Aliases: []string{"i"},
-		Usage:   "Builds your Spice program and installs it to a directory in the PATH variable",
-		Flags: []cli.Flag{
-			&cli.BoolFlag{Name: "debug-output", Aliases: []string{"d"}, Usage: "Print compiler output for debugging", Value: false},
-			&cli.IntFlag{Name: "opt-level", Aliases: []string{"o"}, Usage: "Set optimization level", Value: 2},
-		},
-		Action: func(c *cli.Context) error {
-			cmd.Install(c.Args().Get(0), c.Bool("debug-output"), c.Int("opt-level"))
-			return nil
-		},
-	}
-	runCommand := cli.Command{
-		Name:    "run",
-		Aliases: []string{"r"},
-		Usage:   "Builds your Spice program and runs it immediately",
-		Flags: []cli.Flag{
-			&cli.BoolFlag{Name: "debug-output", Aliases: []string{"d"}, Usage: "Print compiler output for debugging", Value: false},
-			&cli.IntFlag{Name: "opt-level", Aliases: []string{"o"}, Usage: "Set optimization level", Value: 2},
-		},
-		Action: func(c *cli.Context) error {
-			cmd.Run(c.Args().Get(0), c.Bool("debug-output"), c.Int("opt-level"))
-			return nil
-		},
-	}
-	uninstallCommand := cli.Command{
-		Name:    "uninstall",
-		Aliases: []string{"u"},
-		Usage:   "Deletes an installed executable from the binary directory",
-		Action: func(c *cli.Context) error {
-			cmd.Uninstall(c.Args().Get(0))
-			return nil
-		},
-	}
-
-	commands := []*cli.Command{&buildCommand, &runCommand, &uninstallCommand}
-	if !util.IsDockerized() {
-		commands = append(commands, &installCommand)
-	}
-
 	// Main cli configuration
 	app := &cli.App{
 		Name:    "spice",
 		Version: util.BuildVersion(version, commit, date, builtBy),
 		Authors: []*cli.Author{
-			{Name: "Marc Auberer", Email: "marc.auberer@chillibits.com"},
+			{
+				Name:  "Marc Auberer",
+				Email: "marc.auberer@chillibits.com",
+			},
 		},
-		Copyright:              "© 2021 Marc Auberer",
-		Usage:                  "Official CLI for the Spice programming language.",
-		Commands:               commands,
 		UseShortOptionHandling: true,
+		EnableBashCompletion:   true,
+		Copyright:              "© 2021 Spice Contributors",
+		Usage:                  "Official CLI for the Spice programming language.",
+		Commands:               cmd.CliCommands,
 	}
 
 	err := app.Run(os.Args)
