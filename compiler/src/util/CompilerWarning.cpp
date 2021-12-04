e6ecbc4394e26c5112e4ac0f259e3f71bca8c164
Marc Auberer <marc.auberer@chillibits.com>
2021-12-04 17:24:45 +0100

    Add CompilerWarning class

---
+++ compiler/src/util/CompilerWarning.cpp
@@ -0,0 +1,40 @@
+// Copyright (c) 2021 ChilliBits. All rights reserved.
+
+#include "CompilerWarning.h"
+
+/**
+ * Constructor: Used in case that the exact code position where the warning occurred is known
+ *
+ * @param token Syntax token, where the warning occurred
+ * @param type Type of the warning
+ * @param message Warning message suffix
+ */
+CompilerWarning::CompilerWarning(const antlr4::Token& token, CompilerWarningType type, const std::string& message) {
+    auto codeLoc = std::to_string(token.getLine()) + ":" + std::to_string(token.getCharPositionInLine() + 1);
+    warningMessage = "Warning at " + codeLoc + ": " + getMessagePrefix(type) + ": " + message;
+}
+
+/**
+ * Print the compiler warning to the standard error output
+ */
+void CompilerWarning::print() {
+    std::cerr << warningMessage << std::endl;
+}
+
+/**
+ * Get the prefix of the warning message for a particular error
+ *
+ * @param type Type of the warning
+ * @return Prefix string for the warning type
+ */
+std::string CompilerWarning::getMessagePrefix(CompilerWarningType type) {
+    switch (type) {
+        case UNUSED_FUNCTION:
+            return "Unused function";
+        case UNUSED_PROCEDURE:
+            return "Unused procedure";
+        case UNUSED_VARIABLE:
+            return "Unused variable";
+    }
+    return "Unknown warning";
+}
---
+++ compiler/src/util/CompilerWarning.h
@@ -0,0 +1,29 @@
+// Copyright (c) 2021 ChilliBits. All rights reserved.
+
+#pragma once
+
+#include "string"
+#include <TokenSource.h>
+#include <Token.h>
+
+enum CompilerWarningType {
+    UNUSED_FUNCTION,
+    UNUSED_PROCEDURE,
+    UNUSED_VARIABLE
+};
+
+/**
+ * Compiler warning template engine
+ */
+class CompilerWarning {
+public:
+    // Constructors
+    explicit CompilerWarning(const antlr4::Token& token, CompilerWarningType type, const std::string& message);
+
+    void print();
+private:
+    // Members
+    std::string warningMessage{};
+
+    static std::string getMessagePrefix(CompilerWarningType);
+};
