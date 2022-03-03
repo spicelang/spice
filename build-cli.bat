5c2726f318c49309397393fdbceb364449c4127f
Marc Auberer <marc.auberer@sap.com>
2022-03-03 20:30:22 +0100

    Add zlib1.dll to installer

--- build-cli.bat
+++ build-cli.bat
@@ -2,8 +2,6 @@
 mkdir bin 2> NUL
 
 cd cli
-go env -w GOOS=windows
-go env -w GOARCH=amd64
 go build -o ../bin/spice.exe .
 
 cd ..
--- build.bat
+++ build.bat
@@ -2,12 +2,10 @@
 mkdir bin 2> NUL
 
 cd cli
-go env -w GOOS=windows
-go env -w GOARCH=amd64
 go build -o ../bin/spice.exe .
 
 cd ../bin
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -G Ninja ../compiler
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -GNinja -DCMAKE_CXX_FLAGS_RELEASE="-O2" ../compiler
 cmake --build . --target Spice_run
 move src\Spice_run.exe spicec.exe
 
