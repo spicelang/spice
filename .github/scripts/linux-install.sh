070c28c65a4880a431100e85383c93013bf96967
Marc Auberer <marc.auberer@chillibits.com>
2022-03-22 22:19:40 +0100

    Fix Dockerfile to use the latest gcc

---
+++ .github/scripts/linux-install.sh
@@ -0,0 +1,4 @@
+sudo apt install build-essential software-properties-common
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt update && sudo apt install gcc-11 g++-11
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 110 --slave /usr/bin/g++ g++ /usr/bin/g++-11 --slave /usr/bin/gcov gcov /usr/bin/gcov-11 --slave /usr/bin/gcc-ar gcc-ar /usr/bin/gcc-ar-11 --slave /usr/bin/gcc-ranlib gcc-ranlib /usr/bin/gcc-ranlib-11
