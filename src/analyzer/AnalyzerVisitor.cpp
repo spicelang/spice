0d268e103c6fcca229adc2d4916b1c040672efa8
Marc Auberer <marc.auberer@sap.com>
2022-07-14 22:44:25 +0200

    Also do dtor call when meeting return stmt

--- src/analyzer/AnalyzerVisitor.cpp
+++ src/analyzer/AnalyzerVisitor.cpp
@@ -1085,6 +1085,7 @@ std::any AnalyzerVisitor::visitImportStmt(SpiceParser::ImportStmtContext *ctx) {
 
 std::any AnalyzerVisitor::visitReturnStmt(SpiceParser::ReturnStmtContext *ctx) {
   SymbolTableEntry *returnVariable = currentScope->lookup(RETURN_VARIABLE_NAME);
+  SymbolType returnType;
   if (returnVariable) { // Return variable => function
     expectedType = returnVariable->getType();
 
@@ -1115,13 +1116,21 @@ std::any AnalyzerVisitor::visitReturnStmt(SpiceParser::ReturnStmtContext *ctx) {
                      "Return without value, but result variable is not initialized yet");
     returnVariable->setUsed();
 
-    return returnVariable->getType();
+    returnType = returnVariable->getType();
+  } else {
+    // No return variable => procedure
+    if (ctx->assignExpr())
+      throw err->get(*ctx->assignExpr()->start, RETURN_WITH_VALUE_IN_PROCEDURE,
+                     "Return statements in procedures may not have a value attached");
+    returnType = SymbolType(TY_DYN);
   }
-  // No return variable => procedure
-  if (ctx->assignExpr())
-    throw err->get(*ctx->assignExpr()->start, RETURN_WITH_VALUE_IN_PROCEDURE,
-                   "Return statements in procedures may not have a value attached");
-  return SymbolType(TY_DYN);
+
+  // Call destructors for variables, that are going out of scope
+  std::vector<SymbolTableEntry *> varsToDestruct = currentScope->getVarsGoingOutOfScope(true);
+  for (SymbolTableEntry *varEntry : varsToDestruct)
+    insertDestructorCall(*ctx->start, varEntry);
+
+  return returnType;
 }
 
 std::any AnalyzerVisitor::visitBreakStmt(SpiceParser::BreakStmtContext *ctx) {
