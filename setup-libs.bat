b4da685940b99387687d3662bfbf8019786fc6f0
Marc Auberer <marc.auberer@chillibits.com>
2022-08-08 18:18:47 +0200

    Switch to LLVM 15.0.0-rc1 again

--- setup-libs.bat
+++ setup-libs.bat
@@ -5,6 +5,8 @@ pushd lib
 
 git clone --depth 1 --branch 4.10.1 https://github.com/antlr/antlr4.git
 
+git clone --depth 1 --branch release-1.12.1 https://github.com/google/googletest.git
+
 mkdir json
 curl -SsL "https://github.com/nlohmann/json/releases/download/v3.11.1/json.hpp" --output json/json.hpp
 
