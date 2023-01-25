9dbf076d4e89c3c5c76a578440915348af14fe35
Marc Auberer <marc.auberer@chillibits.com>
2023-01-25 02:45:18 +0100

    Upgrade Google Test to 1.13.0

--- setup-libs.sh
+++ setup-libs.sh
@@ -5,7 +5,7 @@ cd lib || exit
 
 git clone --depth 1 --branch 4.11.1 https://github.com/antlr/antlr4.git
 
-git clone --depth 1 --branch release-1.12.1 https://github.com/google/googletest.git
+git clone --depth 1 --branch v1.13.0 https://github.com/google/googletest.git
 
 mkdir json
 curl -SsL "https://github.com/nlohmann/json/releases/download/v3.11.2/json.hpp" --output json/json.hpp
