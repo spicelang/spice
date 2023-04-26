79fea73d3edc609f5891ad3e71e760f2eb279b7e
Marc Auberer <marc.auberer@chillibits.com>
2023-04-26 21:54:23 +0200

    Fix wrong vtable

--- src/util/RawStringOStream.cpp
+++ src/util/RawStringOStream.cpp
@@ -2,13 +2,31 @@
 
 #include "RawStringOStream.h"
 
+namespace spice::compiler {
+
+RawStringOStream::RawStringOStream(std::string &output) : llvm::raw_pwrite_stream(true), output(output) {}
+
+/*size_t RawStringOStream::preferred_buffer_size() const {
+#ifdef _WIN32
+  // On Windows BUFSIZ is only 512 which results in more calls to write. This
+  // overhead can cause significant performance degradation. Therefore, use a
+  // better default.
+  return 16 * 1024;
+#else
+  // BUFSIZ is intended to be a reasonable default.
+  return BUFSIZ;
+#endif
+}
+
+RawStringOStream::~RawStringOStream() { flush(); }*/
+
 void RawStringOStream::pwrite_impl(const char *Ptr, size_t Size, uint64_t Offset) {
   assert(output.length() >= Offset + Size);
   std::memcpy((void *)(output.c_str() + Offset), Ptr, Size);
 }
 
-RawStringOStream::~RawStringOStream() { flush(); }
-
 void RawStringOStream::write_impl(const char *Ptr, size_t Size) { output.append(Ptr, Size); }
 
 uint64_t RawStringOStream::current_pos() const { return output.size(); }
+
+} // namespace spice::compiler
--- src/util/RawStringOStream.h
+++ src/util/RawStringOStream.h
@@ -4,11 +4,21 @@
 
 #include <llvm/Support/raw_ostream.h>
 
+namespace spice::compiler {
+
 class RawStringOStream : public llvm::raw_pwrite_stream {
 public:
   // Public constructors
-  explicit RawStringOStream(std::string &output) : llvm::raw_pwrite_stream(true), output(output) {}
-  virtual ~RawStringOStream() override;
+  explicit RawStringOStream(std::string &output);
+
+  // Public destructor
+  // virtual ~RawStringOStream() override;
+
+  // Public methods
+  /*raw_ostream &changeColor(enum Colors Color, bool Bold, bool BG) override { return *this; }
+  raw_ostream &resetColor() override { return *this; }
+  raw_ostream &reverseColor() override { return *this; }
+  [[nodiscard]] size_t preferred_buffer_size() const override;*/
 
 private:
   // Private members
@@ -19,3 +29,5 @@ private:
   void write_impl(const char *Ptr, size_t Size) override;
   [[nodiscard]] uint64_t current_pos() const override;
 };
+
+} // namespace spice::compiler
