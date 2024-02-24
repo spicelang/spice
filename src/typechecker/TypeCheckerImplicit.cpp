d9d4e3157a0bd11af7965f0932b4a3bf1e389cf6
Marc Auberer <marc.auberer@chillibits.com>
2024-02-24 12:43:10 +0100

    Fix destructing order

--- src/typechecker/TypeCheckerImplicit.cpp
+++ src/typechecker/TypeCheckerImplicit.cpp
@@ -275,7 +275,11 @@ void TypeChecker::implicitlyCallStructDtor(SymbolTableEntry *entry, StmtLstNode 
 void TypeChecker::doScopeCleanup(StmtLstNode *node) {
   // Get all variables, that are approved for deallocation
   std::vector<SymbolTableEntry *> vars = currentScope->getVarsGoingOutOfScope();
-  for (SymbolTableEntry *var : std::ranges::reverse_view(vars)) {
+  // Sort by reverse declaration order
+  auto lambda = [](const SymbolTableEntry *a, const SymbolTableEntry *b) { return a->declNode->codeLoc > b->declNode->codeLoc; };
+  std::ranges::sort(vars, lambda);
+  // Call dtor for each variable. We call the dtor in reverse declaration order
+  for (SymbolTableEntry *var : vars) {
     // Only generate dtor call for structs and if not omitted
     if (!var->getType().is(TY_STRUCT) || var->omitDtorCall)
       continue;
