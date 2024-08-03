f39c60cd41d6b3b85b836bd5ea67b709f9108c49
Marc Auberer <marc.auberer@chillibits.com>
2024-08-03 21:49:57 +0200

    Upgrade ANTLR to 4.13.2

--- setup-libs.bat
+++ setup-libs.bat
@@ -7,7 +7,7 @@ if not exist lib (
 pushd lib
 
 :: Download ANTLR4
-git clone --quiet --depth 1 --branch 4.13.1 https://github.com/antlr/antlr4.git
+git clone --quiet --depth 1 --branch 4.13.2 https://github.com/antlr/antlr4.git
 
 :: Download Google Test
 git clone --quiet --depth 1 --branch v1.15.2 https://github.com/google/googletest.git
