ff0ba7becae3d5f5b1474ac4c80093711f6e9f6c
Marc Auberer <marc.auberer@chillibits.com>
2024-10-05 15:24:52 +0200

    Remove security options and ensure security in CI (#666)

--- Options.cmake
+++ Options.cmake
@@ -137,21 +137,3 @@ if (SPICE_OVERLOAD_NEW_DELETE)
 else ()
     message(STATUS "Spice: New and delete operators are not overloaded")
 endif ()
-
-# RELRO (relocation read-only) security hardening
-option(SPICE_RELRO "Enable relocation read-only (RELRO) hardening" ON)
-if (SPICE_RELRO)
-    message(STATUS "Spice: RELRO hardening enabled (release build only)")
-    set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -Wl,-z,relro,-z,now")
-else ()
-    message(STATUS "Spice: RELRO hardening disabled")
-endif ()
-
-# Stack canary security hardening
-option(SPICE_STACK_PROTECTION "Enable stack canary security hardening" ON)
-if (SPICE_STACK_PROTECTION)
-    message(STATUS "Spice: Stack canaries enabled (release build only)")
-    set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -fstack-protector")
-else ()
-    message(STATUS "Spice: Stack canaries disabled")
-endif ()
