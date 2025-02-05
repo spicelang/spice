989fdcb9ea229698d6e7bb60eef59f4085b28115
Marc Auberer <marc.auberer@chillibits.com>
2025-02-05 01:32:44 +0100

    Fix build error on Windows (#759)

--- src/visualizer/DependencyGraphVisualizer.cpp
+++ src/visualizer/DependencyGraphVisualizer.cpp
@@ -21,14 +21,14 @@ void DependencyGraphVisualizer::getDependencyGraphNode(std::stringstream& output
 
   // Append code for this source file
   const std::filesystem::path relativePath = relative(currentSourceFile->filePath, sourceFile->filePath).generic_string();
-  output << " \"" << relativePath.c_str() << "\" [color=\"lightgreen\",label=\"" << currentSourceFile->fileName << "\"];\n";
+  output << " \"" << relativePath.string() << "\" [color=\"lightgreen\",label=\"" << currentSourceFile->fileName << "\"];\n";
   printedFiles.insert(currentSourceFile);
 
   // Append code for dependencies
   for (const SourceFile *dependency : currentSourceFile->dependencies | std::views::values) {
     getDependencyGraphNode(output, dependency);
     const std::filesystem::path dependencyPathRelative = relative(dependency->filePath, sourceFile->filePath).generic_string();
-    output << " \"" << relativePath.string() << "\" -> \"" << dependencyPathRelative.c_str() << "\";\n";
+    output << " \"" << relativePath.string() << "\" -> \"" << dependencyPathRelative.string() << "\";\n";
   }
 }
 
