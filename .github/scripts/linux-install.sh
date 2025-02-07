f4acced373201cb421b8a6d9fffd3c975b18a90c
Marc Auberer <marc.auberer@chillibits.com>
2025-02-07 19:12:40 +0100

    Use consistent dependency versions in CI

--- .github/scripts/linux-install.sh
+++ .github/scripts/linux-install.sh
@@ -1,4 +1,4 @@
 sudo apt install build-essential software-properties-common
 sudo add-apt-repository ppa:ubuntu-toolchain-r/ppa
-sudo apt update && sudo apt install gcc-13 g++-13
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-13 110 --slave /usr/bin/g++ g++ /usr/bin/g++-13 --slave /usr/bin/gcov gcov /usr/bin/gcov-13 --slave /usr/bin/gcc-ar gcc-ar /usr/bin/gcc-ar-13 --slave /usr/bin/gcc-ranlib gcc-ranlib /usr/bin/gcc-ranlib-13
+sudo apt update && sudo apt install gcc-14 g++-14
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-14 110 --slave /usr/bin/g++ g++ /usr/bin/g++-14 --slave /usr/bin/gcov gcov /usr/bin/gcov-14 --slave /usr/bin/gcc-ar gcc-ar /usr/bin/gcc-ar-14 --slave /usr/bin/gcc-ranlib gcc-ranlib /usr/bin/gcc-ranlib-14
