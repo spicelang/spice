783fbee5a6562f13d396116ea7e5f78473d71d14
Marc Auberer <marc.auberer@chillibits.com>
2025-04-05 12:04:57 +0200

    Upgrade to LLVM 20.1.2 (#826)

--- dev-setup.bat
+++ dev-setup.bat
@@ -34,7 +34,7 @@ echo done.
 
 :: Clone LLVM
 echo [Step 2] Cloning LLVM (Could take a while) ...
-git clone --depth 1 --branch llvmorg-20.1.1 https://github.com/llvm/llvm-project llvm
+git clone --depth 1 --branch llvmorg-20.1.2 https://github.com/llvm/llvm-project llvm
 echo done.
 
 :: Build LLVM
