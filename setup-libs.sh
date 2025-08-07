9abdcfc6c77f90799dc259bd4e0dd5899c897cc4
Marc Auberer <marc.auberer@chillibits.com>
2025-08-07 22:38:16 +0200

    Upgrade to CMake 4.0.0

--- setup-libs.sh
+++ setup-libs.sh
@@ -6,7 +6,7 @@ fi
 cd lib || exit
 
 # Download ANTLR4
-git clone --quiet --depth 1 --branch 4.13.2 https://github.com/antlr/antlr4.git
+git clone --quiet --depth 1 --branch dev https://github.com/spicelang/antlr4.git
 
 # Download Google Test
 git clone --quiet --depth 1 --branch v1.17.0 https://github.com/google/googletest.git
