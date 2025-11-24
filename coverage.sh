699f63d63e72806b14d3d7d95abdc037959ef875
Marc Auberer <marc.auberer@sap.com>
2025-11-24 22:12:20 +0100

    Fix coverage exclude pattern (#964)

--- coverage.sh
+++ coverage.sh
@@ -2,5 +2,5 @@
 set -eu
 
 mkdir coverage
-gcovr --exclude-lines-by-pattern "assert(" --html --html-details -s -r . -o coverage/index.html
-gcovr --exclude-lines-by-pattern "assert(" --txt -r . -o coverage.txt
+gcovr --exclude-lines-by-pattern "assert" --html --html-details -s -r . -o coverage/index.html
+gcovr --exclude-lines-by-pattern "assert" --txt -r . -o coverage.txt
