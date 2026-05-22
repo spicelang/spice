ec0df5e8e06142b831bd2fb1a1b47b46a95027c2
Marc Auberer <marc.auberer@sap.com>
2026-05-22 23:28:14 +0200

    Fix macOS build in CI (#1112)

--- Conditionals.cmake
+++ Conditionals.cmake
@@ -10,3 +10,9 @@ endif()
 
 # Set release flags
 set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -O3 -fdata-sections -ffunction-sections ${GC_SECTIONS_FLAG} -fvisibility=hidden")
+
+# On macOS, libc++ marks std::wstring_convert (used by CLI11) as deprecated. Keep it a warning, not an error.
+set(SPICE_EXTRA_COMPILE_OPTIONS "")
+if (APPLE)
+    list(APPEND SPICE_EXTRA_COMPILE_OPTIONS -Wno-error=deprecated-declarations)
+endif()
